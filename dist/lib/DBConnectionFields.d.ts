a4bc83774d63ca29990370afec6698d20a21c166
Priyanka Yadav <priyankanyadav1144@gmail.com>
2026-06-21 16:06:01 -0400

    Remove .env and dist from tracking

--- dist/lib/DBConnectionFields.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class DBConnectionFields {
-    host: string;
-    user: string;
-    password: string;
-    database: string;
-    constructor(host: string, user: string, password: string, database: string);
-}
-//# sourceMappingURL=DBConnectionFields.d.ts.map
